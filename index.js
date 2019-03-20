9346199f80b36554a346c950976f53d4f0ee88af
Yasmin Torres <yasmintorres90@hotmail.com>
2019-03-20 14:50:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 // Code your solution here!
 
+let myString = 'pizza';
+console.log(myString[0]);
+
+ 
+myString = 'izza';
+console.log(myString[0]);
+
+ 
+myString = 'zza';
+
+
+
 function printString(myString) {
   console.log(myString[0]);
  

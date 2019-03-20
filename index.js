552744a4d5b0459535e587f0fdd02dda707e0404
Yasmin Torres <yasmintorres90@hotmail.com>
2019-03-20 14:51:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ myString = 'zza';
 function printString(string) {
   let substring;
  
-  // Print out the current first character in the string.
+ 
   console.log(string[0]);
  
-  // Store the remainder of the string in the 'substring' variable.
+ 
   substring = string.substring(1, string.length);
  
-  // Invoke printString() from within, passing in the remainder of the previous string.
+  
   printString(substring);
 }
  

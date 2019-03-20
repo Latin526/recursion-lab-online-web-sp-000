2d88b8d27479f726bb4ab5742bcfe6c3d517e037
Yasmin Torres <yasmintorres90@hotmail.com>
2019-03-20 14:49:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // Code your solution here!
 
 function printString(myString) {
-  console.log(myString[0], myString[1], myString[2], myString[3], myString[4]);
-}
+  console.log(myString[0]);
  
-printString("pizza");
-
-printString("oops");
-
-printString("That's not good...");
+  if (myString.length > 1) {
+    let mySubString = myString.substring(1, myString.length);
+    printString(mySubString);
+  } else {
+    return true;
+  }
+}

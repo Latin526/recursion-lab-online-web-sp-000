5c852a53a3340742ee587ef88850ea42e28314d9
Yasmin Torres <yasmintorres90@hotmail.com>
2019-03-20 14:52:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,31 +1,5 @@
 // Code your solution here!
 
-let myString = 'pizza';
-console.log(myString[0]);
-
- 
-myString = 'izza';
-console.log(myString[0]);
-
- 
-myString = 'zza';
-
-function printString(string) {
-  let substring;
- 
- 
-  console.log(string[0]);
- 
- 
-  substring = string.substring(1, string.length);
- 
-  
-  printString(substring);
-}
- 
-printString("pizza");
-
-
 function printString(myString) {
   console.log(myString[0]);
  

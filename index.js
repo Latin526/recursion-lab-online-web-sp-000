eaba5dea92e84b66e587ef8a5c1d7d8ae919689c
Yasmin Torres <yasmintorres90@hotmail.com>
2019-03-20 14:45:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,21 +1,2 @@
 // Code your solution here!
 
-const fact = n => {
-  return n <= 1 ? 1 : n*fact(n-1)
-}
-
-const sum = arr => {
-  const n = arr.length;
-  if(n === 0){
-    return 0;
-  } else if(n == 1){
-    return arr[0];
-  } else if(n == 2){
-    return arr[0] + arr[1];
-  } else {
-    const mid = Math.floor(n / 2);
-    const left = arr.slice(0,mid)
-    const right = arr.slice(mid,n)
-    return sum(left) + sum(right)
-  }
-}
